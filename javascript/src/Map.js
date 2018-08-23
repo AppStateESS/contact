6d9e0deffbf91d088cebd3f970b9860105ac0fa3
Matthew McNaney <mcnaney@gmail.com>
2018-08-23 13:15:17 -0400

    Added lat/long adjustment for thumbnail
    Resolves #7

--- javascript/src/Map.js
+++ javascript/src/Map.js
@@ -14,19 +14,25 @@ class Map extends Component {
     this.state = {
       thumbAllowed: false,
       fullAddress: null,
-      accessToken: null
+      accessToken: null,
+      latitude: 0,
+      longitude: 0,
     }
   }
 
   componentDidMount() {
     const {street, city, state, zip,} = this.props.settings
     const thumbAllowed = (street.length > 0 && city.length > 0 && state.length > 0)
-    //const fullState = this.getFullStateName(state)
     const fullAddress = `${street}, ${city}, ${state}, ${zip}`
-    this.setState(
-      {thumbAllowed, fullAddress, accessToken: this.props.settings.accessToken,}
-    )
+    const {latitude, longitude,} = this.props.settings
 
+    this.setState({
+      thumbAllowed,
+      fullAddress,
+      accessToken: this.props.settings.accessToken,
+      latitude,
+      longitude,
+    })
   }
 
   accessTokenForm() {
@@ -88,27 +94,39 @@ class Map extends Component {
     return $.getJSON(full)
   }
 
+  saveThumbnail() {
+
+    $.getJSON('./contact/admin/map/saveThumbnail', {
+      latitude: this.state.latitude,
+      longitude: this.state.longitude,
+      dimensions: this.props.settings.dimensions,
+      pitch: this.props.settings.pitch,
+      zoom: this.props.settings.zoom,
+    }).done(function (data) {
+      const imageUrl = data.image
+      this.props.update('thumbnail_map', imageUrl)
+    }.bind(this))
+  }
+
   getThumbnail() {
     const promise = this.getGeo()
     promise.done((data) => {
       const latitude = data[0].lat
       const longitude = data[0].lon
-      $.getJSON('./contact/admin/map/saveThumbnail', {
-        latitude: latitude,
-        longitude: longitude,
-        dimensions: this.props.settings.dimensions,
-        pitch : this.props.settings.pitch,
-        zoom: this.props.settings.zoom,
-      }).done(function (data) {
-        const imageUrl = data.image
-        this.props.update('thumbnail_map', imageUrl)
-      }.bind(this))
+      this.setState({
+        latitude,
+        longitude
+      }, () => this.saveThumbnail())
     })
   }
-
+  
   saveAccessToken() {
     this.props.update('accessToken', this.state.accessToken)
     this.props.saveAccessToken()
+  }
+
+  useLatLong() {
+    this.saveThumbnail()
   }
 
   thumbnailForm() {
@@ -116,53 +134,97 @@ class Map extends Component {
       return null
     }
     let thumbnail
-    let getThumbnailButton = <button className="btn btn-primary" onClick={() => this.getThumbnail()}>Create new thumbnail</button>
+    let getThumbnailButton = <button className="btn btn-primary mr-2" onClick={() => this.getThumbnail()}>Create thumbnail from address</button>
+
     let clearThumbnailButton
     if (this.props.settings.thumbnail_map) {
       thumbnail = <img src={this.props.settings.thumbnail_map}/>
-      getThumbnailButton = <button className="btn btn-info mr-2" onClick={() => this.getThumbnail()}>Make new thumbnail</button>
+      getThumbnailButton = <button className="btn btn-info mr-2" onClick={() => this.getThumbnail()}>Refresh GPS</button>
       clearThumbnailButton = <button className="btn btn-danger" onClick={() => this.dropThumbnail()}>Remove thumbnail</button>
     }
-    
+
     return (
       <div>
         <div className="row mb-2">
           <div className="col-sm-6">
-            <div className="mb-2">{thumbnail}</div>
-            {getThumbnailButton}{clearThumbnailButton}
+            <h3>Thumbnail settings</h3>
+            <BigCheckbox
+              label="Pitch thumbnail"
+              checked={this.props.settings.pitch === '60'}
+              handle={() => this.props.update('pitch', '60')}/>
+            <div className="mt-4">
+              <div className="row">
+                <div className="col-sm-6">
+                  <h4>Dimensions</h4>
+                  <div>
+                    <label>
+                      <input
+                        type="radio"
+                        name="dimensions"
+                        value="300x200"
+                        checked={this.props.settings.dimensions === '300x200'}
+                        onChange={() => this.props.update('dimensions', '300x200')}/>
+                      <span>300x200</span>
+                    </label>
+                  </div>
+                  <div>
+                    <label>
+                      <input
+                        type="radio"
+                        name="dimensions"
+                        value="300x300"
+                        checked={this.props.settings.dimensions === '300x300'}
+                        onChange={() => this.props.update('dimensions', '300x300')}/>
+                      <span>300x300</span>
+                    </label>
+                  </div>
+                </div>
+                <div className="col-sm-6">
+                  <h4>Zoom level</h4>
+                  <select
+                    className="form-control"
+                    value={this.props.settings.zoom}
+                    onChange={(e) => this.props.update('zoom', e)}>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <label>Latitude</label><br/>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="latitude"
+                  value={this.state.latitude}
+                  onChange={(e) => {
+                    this.setState({latitude: e.target.value})
+                  }}/>
+              </div>
+              <div className="col-6">
+                <label>Longitude</label><br/>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="longitude"
+                  value={this.state.longitude}
+                  onChange={(e) => {
+                    this.setState({longitude: e.target.value})
+                  }}/>
+              </div>
+            </div>
+            <div className="mt-4">
+              {getThumbnailButton}
+              <button className="btn btn-warning" onClick={() => this.useLatLong()}>Create thumbnail from lat/long</button>
+            </div>
           </div>
           <div className="col-sm-6">
-            <h3>Thumbnail settings</h3>
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  name="dimensions"
-                  value="300x200"
-                  checked={this.props.settings.dimensions === '300x200'}
-                  onChange={() => this.props.update('dimensions', '300x200')}/>
-                <span>300x200</span>
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  name="dimensions"
-                  value="300x300"
-                  checked={this.props.settings.dimensions === '300x300'}
-                  onChange={() => this.props.update('dimensions', '300x300')}/>
-                <span>300x300</span>
-              </label>
-            </div>
-            <BigCheckbox label="Pitch thumbnail" checked={this.props.settings.pitch === '60'} handle={()=>this.props.update('pitch', '60')}/>
-            <label>Zoom level</label>
-            <select className="form-control" value={this.props.settings.zoom} onChange={(e)=>this.props.update('zoom',e)}>
-              <option value="15">15</option>
-              <option value="16">16</option>
-              <option value="17">17</option>
-              <option value="18">18</option>
-            </select>
+            <div className="mb-2"><a href={`https://www.openstreetmap.org/#map=16/${this.state.latitude}/${this.state.longitude}`} target="_blank">{thumbnail}</a></div>
+            {clearThumbnailButton}
           </div>
         </div>
       </div>
@@ -184,7 +246,6 @@ class Map extends Component {
         <h3>Current address - {this.state.fullAddress}</h3>
         <hr/> {this.accessTokenForm()}
         <hr/> {this.thumbnailForm()}
-
       </div>
     )
   }
