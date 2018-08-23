dd5f6b416c446e1c7fbfee13c489b2bfbe066ca3
Matthew McNaney <mcnaney@gmail.com>
2018-08-23 09:33:19 -0400

    Script compile pick define.

---
+++ config/defines.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * MIT License
+ * Copyright (c) 2018 Electronic Student Services @ Appalachian State University
+ * 
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ * 
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+define('CONTACT_SCRIPT_PRODUCTION', true);
